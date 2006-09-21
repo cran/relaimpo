a0d5efe487e8a8269166df5d123030716db29d3a
Ulrike Groemping <groemping@tfh-berlin.de>
2006-09-21 00:00:00 +0000

    version 1.1-1

--- R/methods.R
+++ R/methods.R
@@ -3,4 +3,18 @@
 #Method for coercing x of class relimplm to list 
 #with as(x,"list")
 setAs("relimplm","list",as.relimplm)
+setMethod("show",signature(object="relimplm"),function(object) print.relimplm(object))
+setMethod("show",signature(object="relimplmbooteval"),function(object) print.relimplmbooteval(object))
+setMethod("show",signature(object="relimplmboot"),function(object) 
+    {
+    cat("Objects of class relimplmboot should not be printed.", "\n", 
+        "To see their structure, use the function str().", "\n")
+    })
+setMethod("print",signature(x="relimplm"),function(x) print.relimplm(x))
+setMethod("print",signature(x="relimplmbooteval"),function(x) print.relimplmbooteval(x))
+setMethod("print",signature(x="relimplmboot"),function(x) 
+    {
+    cat("Objects of class relimplmboot should not be printed.", "\n", 
+        "To see their structure, use the function str().", "\n")
+    })
 
